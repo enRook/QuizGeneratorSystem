9494dd23cae75c1a690eec0d8b133923b37354cd
enRook <133678305+enRook@users.noreply.github.com>
2024-05-23 16:59:30 +0700

    Add files via upload

---
+++ QuizGenerator/teacher/includes/footer.php
@@ -0,0 +1,4 @@
+ <div style="   position: fixed;left: 0;bottom: 0;width: 100%;background-color: #37474f;color: white;text-align: center;padding-top:15px;">
+
+  <p>© 2024 Quiz Generator | Southern Leyte State University | IT DEP |  slsu.edu.ph</p>
+</div>
